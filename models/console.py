eec4c15dc504c1c74c1459259d0757b600df0eb4
AngeiraT <thaispur04@icloud.com>
2022-10-12 17:09:26 -0400

    do exit command

--- models/console.py
+++ models/console.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
 import cmd
-import models 
+from models.
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Class Command interpreter
+    """
+    intro = ''
+    prompt = '(hbnb) '
+    file = None
+
+   
+    def do_quit(self, line):
+        exit() 
 
 
-
-
-
-
-    if __name__ == '__main__':
+if __name__ == '__main__':
         HBNBCommand().cmdloop()
