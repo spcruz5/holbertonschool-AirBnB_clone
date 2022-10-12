e3eb59efd5af76409ee53c43593864e88c275b14
AngeiraT <thaispur04@icloud.com>
2022-10-11 23:18:38 -0400

    updated task 3 basemodel

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Class BaseModel
+"""
+
+from datetime import datetime
+import uuid
+import models
+# dtm = dateformat
+dtm = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel:
+    """Base Model"""
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            # Check for keys and value in the items
+            for key, value in kwargs.items():
+                # Assign key the actual date of creation
+                if key == "created_at":
+                    self.created_at = datetime.strptime(
+                    value, dtm)
+                # Assign key the updated date
+                elif key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                    value, dtm)
+                # Assign value to key
+                # self: object which attribute will be assigned
+                # key: attribute of the object assigned
+                # value: value which will be assigned to the variable
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            # Assign aleotory id
+            self.id = str(uuid.uuid4)
+            # Assign updated date
+            self.created_at = datetime.now()
+            # Update the last date modification
+            self.updated_at = self.created_at
+            # If is a new instance
+            # not from a dictionary representation
+            models.storage.new(self)
+
+    def __str__(self):
+        """Returns the class name, id and dictionary attrbutes"""
+        return "[{}] ({}) {}" .format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates public instance attribute update_at with the actual date and hour"""
+        self.updated_at = datetime.now()
+        # Call method save(self) of storage
+        models.storage.save()
+
+    def to_dic(self):
+        """Returns a dictionary that contains all the keys / values of instance dictionary"""
+        dic = self.__dir__.copy()
+        dic["created_at"] = self.created_at.strftime(dtm)
+        dic["updated_at"] = self.updated_at.strftime(dtm)
+        dic["__class__"] = self.__class__.__name__
+        return dic
