394775b567eca2a99b0c08427967d8a209a8b819
AngeiraT <thaispur04@icloud.com>
2022-10-12 20:06:26 -0400

    added doshow

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ Creating the command interpreter console
 
 import cmd
 from models.base_model import BaseModel
+import models
 
 
 class HBNBCommand(cmd.Cmd):
@@ -51,6 +52,23 @@ class HBNBCommand(cmd.Cmd):
             instance.save()
         except:
             print("** class doesn't exist **")
+        
+    def do_show(self, line):
+        """Prints an instance as a string based on the class and id"""    
+        className_line = line.split()
+        if len(className_line) == 0:
+            print("** class name missing **")
+            return
+        elif className_line[0] not in classGroup.keys():
+            print("** class doesn't exist **")
+        elif len(className_line) == 1:
+            print("** instance id missing **")
+        elif len(className_line) == 2:
+            instance = className_line[0] + "." + className_line[1]
+            if instance in models.storage.all():
+                print(models.storage.all()[instance])
+            else:
+                print("** no instance found **")
             
 
 
