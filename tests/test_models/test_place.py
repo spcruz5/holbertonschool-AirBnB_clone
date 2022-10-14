bc2340cdf3b0cfb2c2bc5da401394f7473f344b0
AngeiraT <thaispur04@icloud.com>
2022-10-13 23:55:09 -0400

    udpade test modules

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-""" Unnitest for Place class """
-
+""" Unittest for Place class """
 import unittest
+import json
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
 from models.place import Place
-
+from models.state import State
+from models.review import Review
+from models.user import User
+from models.engine.file_storage import FileStorage
 
 class Test_Place(unittest.TestCase):
     def setup():
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-""" Unnitest for Review class """
-
+""" Unittest for Review class """
 import unittest
+import json
+import os
+from datetime import datetime
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
 from models.review import Review
+from models.user import User
+from models.engine.file_storage import FileStorage
+
 
 
 class Test_Review(unittest.TestCase):
