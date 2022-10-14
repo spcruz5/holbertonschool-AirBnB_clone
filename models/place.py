6d3ded1b733e261a63fff1b863fb301e6467337d
spcruz5 <spcruz5@gmail.com>
2022-10-14 00:52:00 -0400

    Fix white space error in place file

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class that represents a place. 
+    """Class that represents a place.
     Attributes:
 
         city_id: string - empty string: it will be the City.id
@@ -17,7 +17,7 @@ class Place(BaseModel):
         price_by_night: integer - 0
         latitude: float - 0.0
         longitude: float - 0.0
-        amenity_ids: list of string - empty list: it will be the list of Amenity.id later
+        amenity_ids: list of str- empty will be the list of Amenity.id later
 
     """
     city_id = ""
