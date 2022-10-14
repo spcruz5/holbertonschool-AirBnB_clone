950d127ba91b14390c53bd0f478446e27872577d
spcruz5 <spcruz5@gmail.com>
2022-10-14 00:53:05 -0400

    Fix white space error in review file

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 class Review(BaseModel):
     """Class that represent a review.
     Attributes:
-   
+
         place_id: string - empty string: it will be the Place.id
         user_id: string - empty string: it will be the User.id
         text: string - empty string
